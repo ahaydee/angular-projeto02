83e06e4dbe49cd4a68c3fae34598e039c9f16ec1
ahaydee <annahaydee01@gmail.com>
2020-10-03 16:37:44 -0300

    Criação da estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HelloComponent},
+    ])
+  ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
